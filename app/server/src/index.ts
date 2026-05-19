3ab1ccbae03b8f88babc723f605eda2b7a6e2f21
Teerath-Pant <teerathpant2@gmail.com>
2026-05-19 11:23:23 +0530

    Backend fixes for deploying in render

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,10 +1,12 @@
 import http from 'http';
 
+const PORT = process.env.PORT || 5002;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Backend server running on port 5002\n');
+  res.end(`Backend server running on port ${PORT}\n`);
 });
 
-server.listen(5002, () => {
-  console.log('Server started and listening on http://localhost:5002/');
+server.listen(PORT, () => {
+  console.log(`Server started and listening on port ${PORT}`);
 });
